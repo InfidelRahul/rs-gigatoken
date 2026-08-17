6011424d4f7de4fc7d5075b0bd91be2bb00886da
InfidelRahul <rahulkmofficial@gmail.com>
2026-08-17 14:48:57 +0000

    now we're simply letting Rust 2024 infer/capture it rather than naming an unnecessary lifetime.

--- src/api.rs
+++ src/api.rs
@@ -160,7 +160,7 @@ impl SubstringMatcher {
     }
 
     /// Return `(pattern_index, start, end)` for each non-overlapping match.
-    pub fn find_iter<'h>(&self, haystack: &'h [u8]) -> impl Iterator<Item = (usize, usize, usize)> {
+    pub fn find_iter(&self, haystack: &[u8]) -> impl Iterator<Item = (usize, usize, usize)> {
         self.automaton
             .find_iter(haystack)
             .map(|m| (m.pattern().as_usize(), m.start(), m.end()))
